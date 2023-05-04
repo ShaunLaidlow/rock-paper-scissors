3ea21b66ad3fc58173da308108e464d9447b2bf2
Shaun <shaun.laidlow@gmail.com>
2023-05-03 19:37:17 -0700

    Add Function To Get User's Choice
    created function that prompts user for an option
    and then assigns their choice a numerical value based
    on that choice

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 function getComputerChoice() {
+    //randomly generate a value from 0 to 1
     let choice = Math.random();
+
     if(choice < 0.33){
-        choice = "Rock!"
+        choice = 0
+        //0 means rock
     }
     else if(choice < 0.66){
-        choice = "Paper!"
+        choice = 1
+        //1 means paper
     }
     else {
-        choice = "Scissors!"
+        choice = 2
+        //2 means scissors
     }
-    console.log(choice);
+    return choice;
 
 }
 
-getComputerChoice();
+function getUserChoice() {
+    let choice = prompt("What are you throwing?").toLowerCase();
+    if(choice == "rock"){
+        choice = 0;
+    }
+    if(choice == "paper"){
+        choice = 1;
+    }
+    if(choice == "scissors"){
+        choice = 2;
+    }
+    return choice;
+}
+
+
+
+
+let user_choice = getUserChoice();
+let computer_choice = getComputerChoice();
+console.log(computer_choice);
