a93acf8e73b1458dbcb42e97d7afd496637aa82f
Shaun <shaun.laidlow@gmail.com>
2023-05-03 20:10:23 -0700

    Reform Game Into Rounds. Create Game of 5 Rounds
    reformed the game to play a round. In the game function
    their are 5 rounds that are played. It decides the winner

--- script.js
+++ script.js
@@ -27,43 +27,74 @@ function getUserChoice() {
 function selectWinner(user,comp){
     console.log("users: "+ user + " computers: " + comp)
     let statement;
+    let winner;
     if(user == comp){
         statement = "It's a Draw"
+        winner = "draw";
     }
     if(user == "rock"){
         if (comp == "paper"){
             statement = "You Lose! Paper beats Rock";
+            winner = "comp";
         }
         if (comp == "scissors"){
             statement = "You Win! Rock beats Scissors";
+            winner = "user";
         }
     }
     if(user == "paper"){
         if(comp== "scissors"){
             statement = "You Lose! Scissors beats Paper";
+            winner = "comp";
         }
         if(comp == "rock"){
             statement = "You Win! Paper beats Rock";
+            winner = "user";
         }
     }
     if(user == "scissors"){
         if(comp== "rock"){
             statement = "You Lose! Rock beats Scissors";
+            winner = "comp";
         }
         if(comp == "paper"){
             statement = "You Win! Scissors beats Paper";
+            winner = "user";
         }
     }
-
-    return statement;
+    console.log(statement)
+    return winner;
 }
 
 
 function playRound(){
     let user_choice = getUserChoice();
     let computer_choice = getComputerChoice();
-    let decision = selectWinner(user_choice,computer_choice)
-    return decision
+    let winner = selectWinner(user_choice,computer_choice)
+    return winner;
 }
 
-console.log(playRound());
+function game(){
+    let user_score = 0;
+    let comp_score = 0;
+    for(let i = 0; i < 5; i++){
+        winner = playRound();
+        if (winner == "user"){
+            user_score++;
+        }
+        else{
+            comp_score++;
+        }
+    }
+    if(user_score > comp_score){
+        return "You Did It!"
+    }
+    else if(user_score < comp_score){
+        return "Try Again"
+    }
+    else{
+        return "It's a Draw"
+    }
+}
+
+console.log(game());
