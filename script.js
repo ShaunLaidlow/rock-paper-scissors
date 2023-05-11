1f181a57ca0a390db41ac42d5fed41a0ed1091d1
Shaun <shaun.laidlow@gmail.com>
2023-05-10 19:33:35 -0700

    ADD GUI TO GAME Added 3 buttons for rock paper scissors. Text below states what the user selected and what the comp selected. Text below that says who is the winner of the round and what the current score is. Winner is first to 5

--- script.js
+++ script.js
@@ -16,49 +16,44 @@ function getComputerChoice() {
 
 }
 
-function getUserChoice() {
-    let choice;
-    while(choice != "rock" && choice != "paper" && choice != "scissors"){
-        choice = prompt("What are you throwing?").toLowerCase();
-    }
-    return choice;
-}
 
 function selectWinner(user,comp){
-    console.log("users: "+ user + " computers: " + comp)
-    let statement;
+    let play = document.querySelector(".play");
+    play.textContent = "users: "+ user + " computers: " + comp;
+
+    let statement = document.querySelector(".results");
     let winner;
     if(user == comp){
-        statement = "It's a Draw"
+        statement.textContent = "It's a Draw"
         winner = "draw";
     }
     if(user == "rock"){
         if (comp == "paper"){
-            statement = "You Lose! Paper beats Rock";
+            statement.textContent = "You Lose! Paper beats Rock";
             winner = "comp";
         }
         if (comp == "scissors"){
-            statement = "You Win! Rock beats Scissors";
+            statement.textContent = "You Win! Rock beats Scissors";
             winner = "user";
         }
     }
     if(user == "paper"){
         if(comp== "scissors"){
-            statement = "You Lose! Scissors beats Paper";
+            statement.textContent = "You Lose! Scissors beats Paper";
             winner = "comp";
         }
         if(comp == "rock"){
-            statement = "You Win! Paper beats Rock";
+            statement.textContent = "You Win! Paper beats Rock";
             winner = "user";
         }
     }
     if(user == "scissors"){
         if(comp== "rock"){
-            statement = "You Lose! Rock beats Scissors";
+            statement.textContent = "You Lose! Rock beats Scissors";
             winner = "comp";
         }
         if(comp == "paper"){
-            statement = "You Win! Scissors beats Paper";
+            statement.textContent = "You Win! Scissors beats Paper";
             winner = "user";
         }
     }
@@ -67,34 +62,46 @@ function selectWinner(user,comp){
 }
 
 
-function playRound(){
-    let user_choice = getUserChoice();
+function playRound(user){
+    let user_choice = user;
     let computer_choice = getComputerChoice();
     let winner = selectWinner(user_choice,computer_choice)
     return winner;
 }
 
-function game(){
-    let user_score = 0;
-    let comp_score = 0;
-    for(let i = 0; i < 5; i++){
-        winner = playRound();
-        if (winner == "user"){
-            user_score++;
-        }
-        else{
-            comp_score++;
-        }
-    }
-    if(user_score > comp_score){
-        return "You Did It!"
-    }
-    else if(user_score < comp_score){
-        return "Try Again"
+function game(winner){
+    let tally = document.querySelector('.tally');
+    if (winner == "user"){
+        user_score++;
     }
     else{
-        return "It's a Draw"
+        comp_score++;
+    }
+    
+    if(user_score > 4){
+        tally.textContent= "You Did It!"
+    }
+    else if(comp_score > 4){
+        tally.textContent= "Try Again"
+    }
+    else{
+        tally.textContent = `User: ${user_score} | Comp: ${comp_score}`
     }
 }
+let user_score = 0;
+let comp_score = 0;
+const rock = document.querySelector('.rock');
+rock.addEventListener('click',() => {
+    let winner = playRound("rock");
+    game(winner);
+})
 
-console.log(game());
+const paper = document.querySelector('.paper');
+paper.addEventListener('click',() => {
+    let winner = playRound("paper");
+})
+
+const scissors = document.querySelector('.scissors');
+scissors.addEventListener('click',() => {
+    let winner = playRound("scissors");
+})
